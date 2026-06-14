ac45cf92044bf47950d69097a9aa79a851ae2437
Ankit Tripathi <ankittripathi559@gmail.com>
2026-06-14 18:15:46 +0000

    Fix nodemailer smtp config

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    message: "Portfolio Backend Running 🚀",
+    message: "Portfolio Backend Running Successfully",
   });
 });
 
@@ -39,12 +39,17 @@ app.post("/api/contact", async (req, res) => {
     });
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
